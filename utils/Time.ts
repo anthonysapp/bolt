10b659c740add38033f563b6cb3c888a29f63c54
Anthony Sapp <anthony.sapp@campjefferson.com>
2016-09-29 15:33:47 -0400

    added countdown capability to Time

--- utils/Time.ts
+++ utils/Time.ts
@@ -8,4 +8,21 @@ export class Time {
             });
         });
     }
+
+    static count(seconds: number, updateCallback: Function, updateContext: any): Promise<{}> {
+        let obj = { seconds: seconds },
+            currentSeconds: number = seconds;
+        return new Promise(function (resolve, reject) {
+            TweenMax.to(obj, seconds, { ease:Linear.easeNone,
+                seconds: 0, roundProps: "seconds", onUpdate: () => {
+                    if (obj.seconds !== currentSeconds) {
+                        currentSeconds = obj.seconds;
+                        if (updateCallback) {
+                            updateCallback.apply(updateContext,[obj.seconds]);
+                        }
+                    }
+                }, onComplete: resolve
+            });
+        });
+    }
 }
