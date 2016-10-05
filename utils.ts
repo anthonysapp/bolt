3c12535df62baff7e9cbc3f893bdd0ee01cab466
Anthony Sapp <anthony.sapp@campjefferson.com>
2016-10-05 17:14:07 -0400

    removed logger and Notifications

--- utils.ts
+++ utils.ts
@@ -1,5 +1,3 @@
 export {Device} from './utils/Device';
-export {Logger} from './utils/Logger';
-export {Notifications} from './utils/Notifications';
 export {Animator} from './utils/Animator';
 export {Time} from './utils/Time';
